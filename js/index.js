b96d8828df0d38929b3ef30b5bfd6c2e29ee3794
Nathan Cooper <ncoop720@yahoo.com>
2017-11-01 18:18:32 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,6 +7,10 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var portfolioItems = [{
+  href: "scientific-calculator/index.html",
+  img: "scientific-calculator/sc_pic.png",
+  text: "Scientific Calculator"
+}, {
   href: "game-of-life/index.html",
   img: "game-of-life/gol_pic.png",
   text: "Mathematician John Conway's Game of Life"
